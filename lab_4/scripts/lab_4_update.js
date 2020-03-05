ff45b11bc9e53291aecd8d0dc03da2954922a81b
Ryan Singh <ryanvsingh998@gmail.com>
2020-03-04 21:46:58 -0500

    update u
    update

--- lab_4/scripts/lab_4_update.js
+++ lab_4/scripts/lab_4_update.js
@@ -1,16 +1,16 @@
 const myTitle = document.querySelector("title");
 const myHeading = document.querySelector("p");
+
 myHeading.textContent = "Hello world!";
 
 myTitle.textContent = "Ryan Singh' Lab_4";
-var br = document.createElement("br");
 
+var br = document.createElement("br");
 var name = "Ryan Singh";
 var first_name = name.substring(0, 4)
 var last_name = name.substring(4, 9)
 var count = name.length - 1
 var counter = 3
-
 var total = first_name.length + last_name.length
 
 const myHeader = document.querySelector(".header");
@@ -25,6 +25,7 @@ paragraph.classList.add("para1");
 var sec_paragraph = document.createElement("p");
 var sec_text = document.createTextNode("The third character in my name is " +name[2].toUpperCase());
 var third_text = document.createTextNode("ros")
+
 sec_paragraph.appendChild(sec_text);
 sec_paragraph.classList.add("para2");
 sec_paragraph.appendChild(br)
