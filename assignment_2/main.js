edcfe7475f705775b09496b8d9132dca1b78b512
Ryan Singh <ryanvsingh998@gmail.com>
2020-03-31 17:37:43 -0400

    asst 2
    asst 2

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -1,13 +1,23 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
+
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+const images = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.png"];
+images.forEach(image => {
+    
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image);
+    
+    newImage.onclick = ()=>{
+        displayedImage.setAttribute('src', image);
+    }
+    
+    thumbBar.appendChild(newImage);
+})
+
 
 /* Wiring up the Darken/Lighten button */
