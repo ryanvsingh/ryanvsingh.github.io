b968ba4f5b34acc7ee1e420553bc039ce73f8b10
Ryan Singh <ryanvsingh998@gmail.com>
2020-04-01 12:00:53 -0400

    update
    update

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -5,19 +5,27 @@ const overlay = document.querySelector('.overlay');
 
 
 /* Looping through images */
+for (let x=i; x<=5; x++) {
 
-const images = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.png"];
-images.forEach(image => {
-    
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', image);
-    
-    newImage.onclick = ()=>{
-        displayedImage.setAttribute('src', image);
+    const newImage = document.createElement("img");
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage)
+    newImage.onclick = function(e) {
+        displayedImage.src = e.target.src;
     }
-    
-    thumbBar.appendChild(newImage);
-})
+}
 
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function() {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.innerHTML = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+}
